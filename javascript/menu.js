e51c052b4ae98af4147a9d556c9959f516762c89
killbib-coder <84342096+killbib-coder@users.noreply.github.com>
2022-12-06 00:03:45 +0100

    update

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,6 +1,8 @@
-let menu = document.querySelector('.menu')
-      let menutoggle = document.querySelector('.toggle');
-      menutoggle.onclick = function(){
-        menutoggle.classList.toggle('active')
-        menu.classList.toggle('active')
-      }
+let menu = document.querySelector('.menu');
+let contenue = document.querySelector('.contenue');
+let menutoggle = document.querySelector('.toggle');
+menutoggle.onclick = function(){
+    menutoggle.classList.toggle('active')
+    menu.classList.toggle('active')
+    contenue.classList.toggle('active')
+}
