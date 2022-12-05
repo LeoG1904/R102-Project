85bc826e5d5b3080f47a159bd1002f3f91bf0aa8
killbib-coder <killbibferrari@gmail.com>
2022-12-05 13:41:10 +0100

    Update

---
+++ javascript/app.js
@@ -0,0 +1,16 @@
+var btn = document.querySelector('.button');
+var nav = document.querySelector('.nav');
+const header = document.querySelector('header');
+
+btn.onclick = function(){
+  nav.classList.toggle('nav_open');
+}
+
+window.onscroll = function(){
+  var top = window.scrollY;
+  if (top >= 30){
+    header.classList.add('active');
+  }else{
+    header.classList.remove('active');
+  }
+}
