228b34891a6338e26efe37d67f7527f054811566
killbib-coder <84342096+killbib-coder@users.noreply.github.com>
2022-12-13 21:48:17 +0100

    Ajout du dark mod

--- javascript/menu.js
+++ javascript/menu.js
@@ -4,3 +4,14 @@ menutoggle.onclick = function(){
     menutoggle.classList.toggle('active')
     menu.classList.toggle('active')
 }
+
+let contenu = document.querySelector('.contenue')
+let header = document.querySelector('header')
+let BlackMode = document.querySelector('.BlackMode');
+BlackMode.onclick = function(){
+    contenu.classList.toggle('Bactive')
+    header.classList.toggle('Bactive')
+    BlackMode.classList.toggle('Bactive')
+    menutoggle.classList.toggle('Bactive')
+    menu.classList.toggle('Bactive')
+}
