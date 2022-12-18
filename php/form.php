e269d84c8fa1dbf420e380cd4ce0647f2f01b063
killbib-coder <killbibferrari@gmail.com>
2022-12-18 22:02:26 +0100

    Dernières modifications + envoie sur dev.killianferrier.fr

--- php/form.php
+++ php/form.php
@@ -11,7 +11,6 @@
   </head>
   <body>
     <header>
-      <!--Bouton pour activer le darkmode du site-->
       <div class="BlackMode">
         <span></span>
         <span></span>
@@ -21,175 +20,177 @@
         <span></span>
         <span></span>
       </div>
-
-      <!--Texte dans le barre de menu fixe-->
       <div id="text">
         <a href="../index.html">Accueil</a>
       </div>
     </header>
-
-    <!--Tout le contenue de la page-->
+    <div class="menu">
+        <a href="#Presentation">Présentation</a>
+        <a href="#Parcours">Parcours</a>
+        <a href="#Pro">Expériences Professionnels</a>
+        <a href="#Passions">Passions</a>
+    </div>
     <div class="contenue">
-      <?php
-      /*---------------------------------------------------------------*/
-      /*
-          Titre : Formulaire d'envoi de mail                                                                            
-          Source:                                                                                                                      
-              URL   : https://phpsources.net/code_s.php?id=57
-              Auteur           : Mathieu                                                                                            
-              Date édition     : 01 Sept 2004                                                                                       
-              Date mise à jour : 13 Sept 2019                                                                                    
-      */
-      /*---------------------------------------------------------------*/?>
-      <form name="contact_form" method="post" action="">
-      <table width="500">
-      <tr>
-      <td valign="top">
-        <label for="nom">Nom *</label>
-      </td>
-      <td valign="top">
-        <input  type="text" name="nom" maxlength="50" size="30" value="<?php if (
-        isset($_POST['nom'])) echo htmlspecialchars($_POST['nom']);?>">
-      </td>
-      </tr>
-      <tr>
-      <td valign="top"">
-        <label for="prenom">Prénom *</label>
-      </td>
-      <td valign="top">
-        <input  type="text" name="prenom" maxlength="50" size="30" value="<?php if
-        (isset($_POST['prenom'])) echo htmlspecialchars($_POST['prenom']);?>">
-      </td>
-      </tr>
-      <tr>
-      <td valign="top">
-        <label for="email">Email Addresse *</label>
-      </td>
-      <td valign="top">
-        <input  type="text" name="email" maxlength="80" size="30" value="<?php if 
-        (isset($_POST['email'])) echo htmlspecialchars($_POST['email']);?>">
-      </td>
-      </tr>
-      <tr>
-      <td valign="top">
-        <label for="telephone">Téléphone</label>
-      </td>
-      <td valign="top">
-        <input  type="text" name="telephone" maxlength="30" size="30" value="
-        <?php if (isset($_POST['telephone'])) echo htmlspecialchars($_POST['telephone'])
-        ;?>">
-      </td>
-      </tr>
-      <tr>
-      <td valign="top">
-        <label for="commentaire">Commentaire *</label>
-      </td>
-      <td valign="top">
-        <textarea  name="commentaire" cols="28" rows="10"><?php if (isset($_POST[
-        'commentaire'])) echo htmlspecialchars($_POST['commentaire']);?></textarea>
-      </td>
-      </tr>
-      <tr>
-      <td colspan="2" style="text-align:center">
-        <input type="submit" value=" Envoyer ">
-      </td>
-      </tr>
-      </table>
-      </form>
-
-    <?php
-      if(isset($_POST['email'])) {
-  
-      // Le destinataire et le sujet du mail
-      $email_to = "killian.ferrier.pro@gmail.com";
-      $email_subject = "Site IUT AIX";
-  
-      function died($error) {
-          // Code d'érreur
-          echo 
-          "Nous sommes désolés, mais des erreurs ont été détectées dans le" .
-          " formulaire que vous avez envoyé. ";
-          echo "Ces erreurs apparaissent ci-dessous.<br /><br />";
-          echo $error."<br /><br />";
-          echo "Merci de corriger ces erreurs.<br /><br />";
-          die();
-      }
-  
-  
-      // si la validation des données attendues existe
-      if(!isset($_POST['nom']) ||
-          !isset($_POST['prenom']) ||
-          !isset($_POST['email']) ||
-          !isset($_POST['telephone']) ||
-          !isset($_POST['commentaire'])) {
-          died(
-          'Nous sommes désolés, mais le formulaire que vous avez soumis semble poser' .
-          ' problème.');
-      }
-  
-      
-  
-      $nom = $_POST['nom']; // required
-      $prenom = $_POST['prenom']; // required
-      $email = $_POST['email']; // required
-      $telephone = $_POST['telephone']; // not required
-      $commentaire = $_POST['commentaire']; // required
-  
-      $error_message = "";
-      $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-  
-      if(!preg_match($email_exp,$email)) {
-        $error_message .= 
-        'L\'adresse e-mail que vous avez entrée ne semble pas être valide.<br />';
-      }
-    
-        // Prend les caractères alphanumériques + le point et le tiret 6
-        $string_exp = "/^[A-Za-z0-9 .'-]+$/";
-    
-      if(!preg_match($string_exp,$nom)) {
-        $error_message .= 
-        'Le nom que vous avez entré ne semble pas être valide.<br />';
-      }
-    
-      if(!preg_match($string_exp,$prenom)) {
-        $error_message .= 
-        'Le prénom que vous avez entré ne semble pas être valide.<br />';
-      }
-    
-      if(strlen($commentaire) < 2) {
-        $error_message .= 
-        'Le commentaire que vous avez entré ne semble pas être valide.<br />';
-      }
-    
-      if(strlen($error_message) > 0) {
-        died($error_message);
-      }
-  
-      $email_message = "Détail.\n\n";
-      $email_message .= "Nom: ".$nom."\n";
-      $email_message .= "Prenom: ".$prenom."\n";
-      $email_message .= "Email: ".$email."\n";
-      $email_message .= "Telephone: ".$telephone."\n";
-      $email_message .= "Commentaire: ".$commentaire."\n";
-  
-      // create email headers
-      $headers = 'From: '.$email."\r\n".
-      'Reply-To: '.$email."\r\n" .
-      'X-Mailer: PHP/' . phpversion();
-      mail($email_to, $email_subject, $email_message, $headers);
-      ?>
+        <section>
+              <?php
+              /*---------------------------------------------------------------*/
+              /*
+                  Titre : Formulaire d'envoi de mail                                                                            
+                  Source:                                                                                                                      
+                      URL   : https://phpsources.net/code_s.php?id=57
+                      Auteur           : Mathieu                                                                                            
+                      Date édition     : 01 Sept 2004                                                                                       
+                      Date mise à jour : 13 Sept 2019                                                                                    
+              */
+              /*---------------------------------------------------------------*/?>
+              <form name="contact_form" method="post" action="">
+              <table width="500">
+              <tr>
+              <td valign="top">
+                <label for="nom">Nom *</label>
+              </td>
+              <td valign="top">
+                <input  type="text" name="nom" maxlength="50" size="30" value="<?php if (
+                isset($_POST['nom'])) echo htmlspecialchars($_POST['nom']);?>">
+              </td>
+              </tr>
+              <tr>
+              <td valign="top"">
+                <label for="prenom">Prénom *</label>
+              </td>
+              <td valign="top">
+                <input  type="text" name="prenom" maxlength="50" size="30" value="<?php if
+                (isset($_POST['prenom'])) echo htmlspecialchars($_POST['prenom']);?>">
+              </td>
+              </tr>
+              <tr>
+              <td valign="top">
+                <label for="email">Email Addresse *</label>
+              </td>
+              <td valign="top">
+                <input  type="text" name="email" maxlength="80" size="30" value="<?php if 
+                (isset($_POST['email'])) echo htmlspecialchars($_POST['email']);?>">
+              </td>
+              </tr>
+              <tr>
+              <td valign="top">
+                <label for="telephone">Téléphone</label>
+              </td>
+              <td valign="top">
+                <input  type="text" name="telephone" maxlength="30" size="30" value="
+                <?php if (isset($_POST['telephone'])) echo htmlspecialchars($_POST['telephone'])
+                ;?>">
+              </td>
+              </tr>
+              <tr>
+              <td valign="top">
+                <label for="commentaire">Commentaire *</label>
+              </td>
+              <td valign="top">
+                <textarea  name="commentaire" cols="28" rows="10"><?php if (isset($_POST[
+                'commentaire'])) echo htmlspecialchars($_POST['commentaire']);?></textarea>
+              </td>
+              </tr>
+              <tr>
+              <td colspan="2" style="text-align:center">
+                <input type="submit" value=" Envoyer ">
+              </td>
+              </tr>
+              </table>
+              </form>
         
-        <!-- mettez ici votre propre message de succès en html -->
+            <?php
+              if(isset($_POST['email'])) {
+          
+              // Le destinataire et le sujet du mail
+              $email_to = "killian.ferrier.pro@gmail.com";
+              $email_subject = "Site IUT AIX";
+          
+              function died($error) {
+                  // Code d'érreur
+                  echo 
+                  "Nous sommes désolés, mais des erreurs ont été détectées dans le" .
+                  " formulaire que vous avez envoyé. ";
+                  echo "Ces erreurs apparaissent ci-dessous.<br /><br />";
+                  echo $error."<br /><br />";
+                  echo "Merci de corriger ces erreurs.<br /><br />";
+                  die();
+              }
+          
+          
+              // si la validation des données attendues existe
+              if(!isset($_POST['nom']) ||
+                  !isset($_POST['prenom']) ||
+                  !isset($_POST['email']) ||
+                  !isset($_POST['telephone']) ||
+                  !isset($_POST['commentaire'])) {
+                  died(
+                  'Nous sommes désolés, mais le formulaire que vous avez soumis semble poser' .
+                  ' problème.');
+              }
+          
+              
+          
+              $nom = $_POST['nom']; // required
+              $prenom = $_POST['prenom']; // required
+              $email = $_POST['email']; // required
+              $telephone = $_POST['telephone']; // not required
+              $commentaire = $_POST['commentaire']; // required
+          
+              $error_message = "";
+              $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+          
+              if(!preg_match($email_exp,$email)) {
+                $error_message .= 
+                'L\'adresse e-mail que vous avez entrée ne semble pas être valide.<br />';
+              }
+            
+                // Prend les caractères alphanumériques + le point et le tiret 6
+                $string_exp = "/^[A-Za-z0-9 .'-]+$/";
+            
+              if(!preg_match($string_exp,$nom)) {
+                $error_message .= 
+                'Le nom que vous avez entré ne semble pas être valide.<br />';
+              }
+            
+              if(!preg_match($string_exp,$prenom)) {
+                $error_message .= 
+                'Le prénom que vous avez entré ne semble pas être valide.<br />';
+              }
+            
+              if(strlen($commentaire) < 2) {
+                $error_message .= 
+                'Le commentaire que vous avez entré ne semble pas être valide.<br />';
+              }
+            
+              if(strlen($error_message) > 0) {
+                died($error_message);
+              }
+          
+              $email_message = "Détail.\n\n";
+              $email_message .= "Nom: ".$nom."\n";
+              $email_message .= "Prenom: ".$prenom."\n";
+              $email_message .= "Email: ".$email."\n";
+              $email_message .= "Telephone: ".$telephone."\n";
+              $email_message .= "Commentaire: ".$commentaire."\n";
+          
+              // create email headers
+              $headers = 'From: '.$email."\r\n".
+              'Reply-To: '.$email."\r\n" .
+              'X-Mailer: PHP/' . phpversion();
+              mail($email_to, $email_subject, $email_message, $headers);
+              ?>
+                
+                <!-- mettez ici votre propre message de succès en html -->
+                
+              Merci de nous avoir contacter. Votre commentaire sera prit en compte.
+              
+              <?php
         
-      Merci de nous avoir contacter. Votre commentaire sera prit en compte.
-      
-      <?php
-
-    }
-    ?>
+            }
+            ?>
+        </section>
     </content>
-
-    <!--Inclusion du java dans la page-->
-    <script src="../javascript/menu.js"></script>
+    <script src="../javascript/blackmode.js"></script>
   </body>
 </html>
